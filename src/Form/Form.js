a4028ded75ed0bf539848f0d149dc41c1f3127a4
Slaven B <slaven.bunijevac@gmail.com>
2021-10-19 13:26:56 +0200

    style the form

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import classes from './Form.module.css';
 
 const Form = () => {
   return (
-    <form>
-      <label htmlFor="input-todo" className="todo-label">
-        <input type="text" className="input-todo" id="input-todo" />
+    <form className={classes.form}>
+      <label htmlFor="input-todo" className={classes.todoLabel}>
+        <input 
+          type="text" 
+          className={classes.todoInput} 
+          id="input-todo"
+          placeholder='Enter new task...' />
       </label>
+      <button className={classes.formBtn}>
+        Add
+      </button>
     </form>
   )
 }
