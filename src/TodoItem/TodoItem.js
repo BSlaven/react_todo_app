ea3b8fe5af1970d13d9a4e7e1a6f5008b10d2d82
Slaven B <slaven.bunijevac@gmail.com>
2021-10-20 08:48:31 +0200

    add todoitem styling, part of it

--- src/TodoItem/TodoItem.js
+++ src/TodoItem/TodoItem.js
@@ -5,6 +5,10 @@ const TodoItem = ({ todo }) => {
   return (
     <div className={classes.todoItem}>
       <p>{todo.title}</p>
+      {/* <div className="controlsContainer"> */}
+        <button className={classes.completeBtn}>+</button>
+        <button className={classes.deleteBtn}>/-</button>
+      {/* </div> */}
     </div>
   )
 }
